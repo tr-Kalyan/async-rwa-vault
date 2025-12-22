bab06cab652f2d3fb04fe08e4aaf414b1b54f8ea
tr-Kalyan <kalyansde1@gmail.com>
2025-12-23 04:10:39 +0530

    chore: pin compiler and OZ version for reliable Sepolia verification
    - Adjust foundry.toml to use Solidity 0.8.24 and compatible OZ version
    - Enables successful contract verification on Sepolia Etherscan
    - Deployed verified vault at 0x9dC96F7D1161F2E40b1D855C57c5FD2a46cFf6b5
    - No functional changes to contract logic

--- src/AsyncSettlementRWAVault.sol
+++ src/AsyncSettlementRWAVault.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.24;
 
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-//import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
